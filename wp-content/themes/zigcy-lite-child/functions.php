48be2a50810486031986445ef10833eec3b57118
noname21062000 <noname21062000@gmail.com>
2019-07-06 10:29:48 +0700

    xong web

--- wp-content/themes/zigcy-lite-child/functions.php
+++ wp-content/themes/zigcy-lite-child/functions.php
@@ -444,19 +444,21 @@ add_shortcode('SHOW_GOOGLEMAP', 'create_footer_two');
 /* THAY ĐỔI 0đ THÀNH LIÊN HỆ*/
 function vbk_wc_custom_get_price_html( $price, $product ) {
     if ( $product->get_price() == 0 ) { 
-
+        
         if ( $product->is_on_sale() && $product->get_regular_price() ) {
             $regular_price = wc_get_price_to_display( $product, array( 'qty' => 1, 'price' => $product->get_regular_price() ) );
 
-            $price = wc_format_price_range( $regular_price, __( 'Free!', 'woocommerce' ) );
+            $price = wc_format_price_range( $regular_price, __( 'Miễn Phí!', 'woocommerce' ) );
         } else {
             $price = '<span class="amount">' . __( 'Liên hệ', 'woocommerce' ) . '</span>'; 
-            
+           
    
         }
     }
     return $price;
 }
 add_filter( 'woocommerce_get_price_html', 'vbk_wc_custom_get_price_html', 10, 2 );
+
+
 
 
